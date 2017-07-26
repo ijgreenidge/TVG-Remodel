74b7484a8a883f683f38af961fb61f48cf096cc8
Imani Greenidge <igreenidge2018@bklawtech.com>
2017-07-26 14:34:22 +0000

    first

---
+++ TVG/assets/theme.js
@@ -0,0 +1,414 @@
+/*--------
+Client List
+---------*/
+    $("#ann-taylor").hover(function() {
+    console.log("Mouse hover function works")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/Pasted-7.jpg?12588535122056963205')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#ann-taylor").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')"); 
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");
+});
+
+    $("#the-childrens-place").hover(function() {
+    // console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/Pasted-6.jpg?12588535122056963205')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#the-childrens-place").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')");
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");
+    
+});
+
+    $("#extra-butter").hover(function() {
+    // console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/Pasted-4.jpg?12588535122056963205')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#extra-butter").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')");
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");
+    
+});
+
+    $("#goat-tape").hover(function() {
+    // console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/Pasted-3.jpg?12588535122056963205')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#goat-tape").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')");
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");
+    
+});
+
+    $("#sustain-natural").hover(function() {
+    // console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/Pasted-5.jpg?12588535122056963205')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+
+    $("#sustain-natural").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')");
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");
+});
+/*-----------
+See More List
+ ----------*/
+    $("#philips").hover(function() {
+    console.log("Mouse hover function works")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/hero-philips.png?8605171945995430636')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#philips").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')"); 
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");
+});
+
+    $("#wandering-bear-coffee").hover(function() {
+    // console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/hero-wandering.png?8605171945995430636')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $(".wandering-bear-coffee").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')");
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");
+    
+});
+
+    $("#coop-home-goods").hover(function() {
+    // console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/hero-coop.png?8605171945995430636')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#coop-home-goods").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')");
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");
+    
+});
+
+    $("#the-shoe-box").hover(function() {
+    // console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/hero-shoebox.png?8605171945995430636')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#the-shoe-box").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')");
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");
+    
+});
+
+    $("#fli-charge").hover(function() {
+    // console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/hero-flicharge.png?8605171945995430636')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#fli-charge").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')");
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");
+    
+});
+    $("#juisi").hover(function() {
+     console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/hero-juisi.png?8605171945995430636')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#juisi").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')"); 
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");
+});   
+    $("#rvs").hover(function() {
+     console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/hero-rvs.png?8605171945995430636')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#rvs").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')"); 
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");
+});  
+    $("#site-speak").hover(function() {
+     console.log("hi")
+    $(".back").css("background-image", "url('https://www.wpfreeware.com/wp-content/uploads/2014/09/placeholder-images.jpg')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#site-speak").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')"); 
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", ""); 
+});  
+/*--------------
+Info List
+--------------*/
+    $("#workspace-willamsburg").hover(function() {
+     console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/Pasted-26.jpg?15571382008131401913')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#workspace-willamsburg").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')"); 
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");  
+});
+
+    $("#testimonials").hover(function() {
+     console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/Testinomials.png?18426293193313692807')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+    $(".back").css("left", "15px");
+});
+    $("#testimonials").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')"); 
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", ""); 
+    $(".back").css("left", "");
+});
+
+    $("#thoughts").hover(function() {
+     console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/Thoughts.png?6656998608906115273')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#thoughts").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')"); 
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");  
+});
+
+    $("#services").hover(function() {
+     console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/Services.png?17450053057900678116')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+    $(".back").css("left", "-10px");
+   
+});
+    $("#services").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')"); 
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", ""); 
+    $(".back").css("left", "");
+});
+
+    $("#squad").hover(function() {
+     console.log("hi")
+    $(".back").css("background-image", "url('https://cdn.shopify.com/s/files/1/2171/8349/files/Squad.png?17450053057900678116')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#squad").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')"); 
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");  
+});
+    $("#shopify-apps").hover(function() {
+     console.log("hi")
+    $(".back").css("background-image", "url('')");
+    $(".back").css("opacity", "0.2");
+    $(".back").css("filter", "grayscale(100%)");
+    $(".back").css("-webkit-background-size", "cover");
+    $(".back").css("-moz-background-size", "cover");
+    $(".back").css("-o-background-size", "cover");
+    $(".back").css("background-size", "cover");
+});
+    $("#shopify-apps").mouseleave(function(){
+    $(".back").css("background-color", "white");
+    $(".back").css("background-image", "url('')"); 
+    $(".back").css("opacity", "1");
+    $(".back").css("filter", "grayscale(0%)");
+    $(".back").css("-webkit-background-size", "");
+    $(".back").css("-moz-background-size", "");
+    $(".back").css("-o-background-size", "");
+    $(".back").css("background-size", "");  
+});
